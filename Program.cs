f98f4d28029957f2a2d73cf7dd30cc6522ff09b0
dk.raw <hello@dkatsios.tk>
2021-03-09 17:34:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity; //needs EntityFramework
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace preview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDatabase Db = new MyDatabase();
+            foreach (var student in Db.Students)
+            {
+                Console.WriteLine($"{student.Name, -15} {student.Identity.Number}");
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public virtual Identity Identity { get; set; }
+    }
+    public class Identity
+    {
+        [ForeignKey("Student")]
+        public int IdentityID { get; set; }
+        public string Number { get; set; }
+        public virtual Student Student { get; set; }
+    }
+    public class MyDatabase : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Identity> Identities { get; set; }
+    }
+}
